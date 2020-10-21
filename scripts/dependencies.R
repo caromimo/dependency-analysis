956956094c852c45dbe7bd720b35561cb8584595
Caroline Mimeault <carolinemimeault@gmail.com>
2020-10-20 22:17:26 -0400

    code to generate clean dataset

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -3,14 +3,28 @@ rm(list = ls())
 
 # libraries
 library(tidyverse)
-library(ggthemes)
-library(extrafont)
+library(lubridate)
 
 # import the datasets
-data <- read_csv("~/projects/dependency-analysis/data/raw/pkgs.csv", col_names = c("package_name", "version", "time_stamp"))
+pkgs <- read_csv("~/projects/dependency-analysis/data/raw/pkgs.csv", col_names = c("package_name", "version", "time_stamp"))
 
-# remove some left over headers
-data <- data %>%
+# remove some left over headers and extract date
+data <- pkgs %>%
   filter(version != "created") %>%
-  filter(version != "modified")
+  filter(version != "modified") %>%
+  mutate(
+    date = as.Date(time_stamp, "%Y/%m/%d"),
+    version = str_replace(version, "(\\d{1,2}.\\d{1,2}.\\d{1,2})(\\w+)", "\\1-\\2")
+  )
 
+# extracting the versions numbers with semver
+data <- add_column(
+  data,
+  pull(
+    data, version) %>%
+    semver::parse_version() %>%
+    as.data.frame() 
+    ) %>%
+  arrange(major, minor, patch)
+
+data %>% write_csv("~/projects/dependency-analysis/data/processed/clean_dataset.csv")
